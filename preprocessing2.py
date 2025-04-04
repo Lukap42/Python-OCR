af3b52b7553fe28395a8364c78080273bae05110
Lukap42 <lukaszp1042@gmail.com>
2025-04-04 18:05:15 +0200

    preprocessing working for 'fox.png'

--- preprocessing2.py
+++ preprocessing2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 
-image_path = "lighting3.png"
+image_path = "fox.png"
 
 def save(image, name):
     cv.imwrite("temp/"+name,image)
@@ -62,19 +62,23 @@ def median(image):
 def gaussian(image):
     return cv.GaussianBlur(image,(3,3),0)
 
-m=cv.medianBlur(gray_image,5)
+#m=cv.medianBlur(gray_image,3)
 #m = median(gray_image)
 #g = gaussian(gray_image)
 #save(m,"median5.png")
 #display2(g,"median")
 
 #Histogram
-hist = cv.equalizeHist(m)
+#hist = cv.equalizeHist(m)
 #display2(hist,"hist")
 
 #Binarization/Thresholding
 
-thresh, binary = cv.threshold(m,110,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C) #110 good
+#f, otsu = cv.threshold(gray_image,70,255,cv.THRESH_OTSU)
+#thresh, binary = cv.threshold(m,110,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C) #110 good
+binary = cv.adaptiveThreshold(gray_image,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,17,6.5) #110 good
+
+display2(binary,"aa1a")
 #display2(binary,"Gauss")
 #save(binary,"lightning3_mean.png")
 
@@ -88,31 +92,37 @@ inverted = cv.bitwise_not(binary)
 #display2(binary)
 
 def dilate(image, k):
-    kernel = np.ones((k,k),np.uint8)
+    kernel = cv.getStructuringElement(cv.MORPH_RECT,(k,k))
     img = cv.dilate(image,kernel,iterations=1)
     return img
 
 def erode(image, k):
-    kernel = np.ones((k,k),np.uint8)
+    kernel = cv.getStructuringElement(cv.MORPH_RECT,(k,k))
     img = cv.erode(image,kernel,iterations=1)
     return img
 
 #Erode + dilate
 def morph(image, k):
-    kernel = np.ones((k,k),np.uint8)
+    kernel = cv.getStructuringElement(cv.MORPH_RECT,(k,k))
     img = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
     return img
 
-#eroded = erode(binary,5)
-#dilated = dilate(eroded,5)
+
+erosion_kernel = cv.getStructuringElement(cv.MORPH_RECT,(3,3))
+erosion_kernel2 = cv.getStructuringElement(cv.MORPH_RECT,(2,2))
+dilate_kernel = cv.getStructuringElement(cv.MORPH_RECT,(9,9))
+
+morphed = cv.erode(inverted,erosion_kernel,iterations=1)
+morphed = cv.dilate(morphed,dilate_kernel,iterations=1)
+
+
+#eroded = erode(inverted,3)
+#dilated = dilate(eroded,9)
 #display2(dilated,"morph")
-#morphed = morph(binary,4)
+#morphed = morph(binary,5)
 #display2(morphed,"morphed")
 
-
-binary = cv.bitwise_not(binary)
-
-display2(binary,"tmp")
+display2(morphed,"tmp")
 
 
 def removeBorder(image):
@@ -123,9 +133,8 @@ def removeBorder(image):
     crop = image[y:y+h,x:x+w]
     return crop
 
-crop = removeBorder(inverted)
+crop = removeBorder(morphed)
 #display2(crop,"crop")
-crop = cv.bitwise_not(crop)
 
 contours, hierarchy = cv.findContours(crop, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
@@ -135,8 +144,6 @@ iter=0
 for cont in contours:
     x,y,w,h = cv.boundingRect(cont)
     rect = cv.rectangle(crop,(x,y),(x+w,y+h),(255,255,255),5)
-    if(h>180 or w>180):
-        iter=iter-1
     #crop = dilated[y:y+h,x:x+w]
     print(iter)
     iter=iter+1
